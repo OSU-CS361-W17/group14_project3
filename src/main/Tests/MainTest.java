754259b896f9ee60954ab54b88e604745e38f44a
Logan H. Miller <millelog@oregonstate.edu>
2017-03-03 19:08:33 -0800

    Added Comments to Describe Testing
    Main is difficult to write automatic tests for given the need for a request object from Spark. Therefore I have written tests that can be checked through the UI and should the majority of the code that is written in Main.java. I'm sorry for the use of github to do this but as it is only comments I felt it was acceptable.

--- src/main/Tests/MainTest.java
+++ src/main/Tests/MainTest.java
@@ -11,11 +11,19 @@ class MainTest {
     }
 
     @Test
+    //This simply creates a fresh model and is first called when the game is started. 
+    //A fresh model should have a completely populated AI board so to make sure the game was initiliazed properly]
+    //Continue to fire at the enemey until you hit a ship
+    //The test is a success if you are able to hit enemy ships
+    //The test is a failure if you hit enough squares on the enemy board to conclude that there are no ships present because you never hit one. 
     void newModel() {
         Main t = new Main();
         assertEquals(true,t.createdNewModel());
     }
-
+    //At first it seemed like this would be used to create a new BattleshipModel for every request
+    //But that didn't fit in with the front-end being stateless.
+    //Therefore the only thing I could think of doing was using GSON to convert a request's text into a BattleshipModel.
+    //It's not clear how this is useful and seems impossible to test.
     @Test
     void getModelFromReq() {
         Main t = new Main();
@@ -33,7 +41,7 @@ class MainTest {
 
     //To test select an x,y coordinate to fire at. Click the fire button in the UI.
     //If a shot appears on the enemy board that is either a hit or miss then the test succeeds.
-    //If nothing appears or it crashes then the test fails
+    //If nothing appears or an alert pops up then the test fails
     //Please see /r/therewasanattempt for more information
     @Test
     void fireAt() {
